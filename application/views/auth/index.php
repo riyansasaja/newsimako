36c779a5fba964e52c59ea92c8695a4b90d3d1d1
riyansasaja <riyanboyuhu@gmail.com>
2021-08-11 18:49:55 +0800

    perbaiki title saat login

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -4,12 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Mazer Admin Dashboard</title>
+    <title>SIMAKO</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/bootstrap.css">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/app.css">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/pages/auth.css">
+
+    <link rel="shortcut icon" href="<?= base_url() ?>assets/images/favicon.png" type="image/x-icon">
 </head>
 
 <body>
