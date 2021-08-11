38ca3db0fd0774d5cd032f4c47c7d0ae88751929
riyansasaja <riyanboyuhu@gmail.com>
2021-08-11 18:45:43 +0800

    hapus info dashboard di atas

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,6 +1,6 @@
 <section class="row">
     <div class="col-12 col-lg-9">
-        <div class="row">
+        <!-- <div class="row">
             <div class="col-6 col-lg-3 col-md-6">
                 <div class="card">
                     <div class="card-body px-3 py-4-5">
@@ -69,7 +69,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="row">
             <div class="col-12">
                 <div class="card">
