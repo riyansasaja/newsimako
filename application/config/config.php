ed4e13202416f2ece19169f376b90295c35aac6c
riyansasaja <riyanboyuhu@gmail.com>
2021-08-11 18:36:42 +0800

    merubah configurasi awal untuk hosting

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/site/newsimako/';
+$config['base_url'] = 'http://manado.simako.site/';
 
 /*
 |--------------------------------------------------------------------------
