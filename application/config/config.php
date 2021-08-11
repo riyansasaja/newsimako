7ea2a852601caa23105661ace6443477ceda2ce2
riyansasaja <riyanboyuhu@gmail.com>
2021-08-11 20:21:03 +0800

    rubah git ignore

--- application/config/config.php
+++ application/config/config.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
-| Base Site URL ini uji coba setelah ignore
+| Base Site URL ini uji coba setelah ignore tes ulang
 |--------------------------------------------------------------------------
 |
 | URL to your CodeIgniter root. Typically this will be your base URL,
